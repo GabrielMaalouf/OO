dfd21fb09d054a4b3b3b86cdaca7316c6bd4fdac
GabrielMaalouf <168315389+GabrielMaalouf@users.noreply.github.com>
2025-04-10 23:56:20 -0300

    Update Pais.cpp

--- Paises/Paises/Pais.cpp
+++ Paises/Paises/Pais.cpp
@@ -1,70 +1,83 @@
 #include "Pais.h"
-#include <algorithm>
+#include <iostream>
 
-using namespace std;
-
-Pais::Pais(const string& iso, const string& nome, double dimensao)
-    : codigoISO(iso), nome(nome), dimensao(dimensao), populacao(0) {
-}
-
-string Pais::getCodigoISO() const {
-    return codigoISO;
-}
-
-void Pais::setCodigoISO(const string& iso) {
-    codigoISO = iso;
-}
-
-string Pais::getNome() const {
-    return nome;
-}
-
-void Pais::setNome(const string& nome) {
+Pais::Pais(string codigo, string nome, double dimensao) {
+    this->codigoISO = codigo;
     this->nome = nome;
-}
-
-long long Pais::getPopulacao() const {
-    return populacao;
-}
-
-void Pais::setPopulacao(long long populacao) {
-    this->populacao = populacao;
-}
-
-double Pais::getDimensao() const {
-    return dimensao;
-}
-
-void Pais::setDimensao(double dimensao) {
     this->dimensao = dimensao;
+    this->populacao = 0;
+    this->totalFronteiras = 0;
+
+    for (int i = 0; i < 40; i++) {
+        fronteiras[i] = nullptr;
+    }
+}
+
+void Pais::setPopulacao(long long pop) {
+    this->populacao = pop;
 }
 
 void Pais::adicionarFronteira(Pais* pais) {
-    if (pais != this && fronteiras.size() < 40 &&
-        find(fronteiras.begin(), fronteiras.end(), pais) == fronteiras.end()) {
-        fronteiras.push_back(pais);
-    }
-}
+    if (pais == this) return;
 
-bool Pais::isEqual(const Pais& outro) const {
-    return this->codigoISO == outro.codigoISO;
-}
-
-bool Pais::isLimitrofe(const Pais& outro) const {
-    return find(fronteiras.begin(), fronteiras.end(), &outro) != fronteiras.end();
-}
-
-double Pais::calcularDensidade() const {
-    if (dimensao == 0) return 0;
-    return static_cast<double>(populacao) / dimensao;
-}
-
-vector<Pais*> Pais::vizinhosEmComum(const Pais& outro) const {
-    vector<Pais*> comuns;
-    for (Pais* pais : fronteiras) {
-        if (outro.isLimitrofe(*pais)) {
-            comuns.push_back(pais);
+    for (int i = 0; i < totalFronteiras; i++) {
+        if (fronteiras[i] == pais) {
+            return; // já está na lista
         }
     }
-    return comuns;
+
+    if (totalFronteiras < 40) {
+        fronteiras[totalFronteiras] = pais;
+        totalFronteiras++;
+    }
+}
+
+string Pais::getNome() {
+    return nome;
+}
+
+string Pais::getCodigoISO() {
+    return codigoISO;
+}
+
+long long Pais::getPopulacao() {
+    return populacao;
+}
+
+double Pais::getDimensao() {
+    return dimensao;
+}
+
+double Pais::calcularDensidade() {
+    if (dimensao == 0) return 0;
+    return populacao / dimensao;
+}
+
+bool Pais::isIgual(Pais* outro) {
+    return this->codigoISO == outro->getCodigoISO();
+}
+
+bool Pais::isLimitrofe(Pais* outro) {
+    for (int i = 0; i < totalFronteiras; i++) {
+        if (fronteiras[i] == outro) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Pais::mostrarVizinhosEmComum(Pais* outro) {
+    cout << "Vizinhos em comum entre " << this->nome << " e " << outro->getNome() << ":\n";
+
+    bool achou = false;
+    for (int i = 0; i < totalFronteiras; i++) {
+        if (fronteiras[i] != nullptr && outro->isLimitrofe(fronteiras[i])) {
+            cout << "- " << fronteiras[i]->getNome() << "\n";
+            achou = true;
+        }
+    }
+
+    if (!achou) {
+        cout << "Nenhum vizinho em comum.\n";
+    }
 }
